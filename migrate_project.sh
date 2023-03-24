f8f867169648ad56295bd3dde90e366bbf4ef4bb
serebano <mr.serebano@gmail.com>
2023-03-24 22:16:07 +0200

    feat: added migrate_project.sh

---
+++ migrate_project.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+#Edit here:
+OLD_DB_URL=db.old_project_ref.supabase.co
+NEW_DB_URL=db.new_project_ref.supabase.co
+
+OLD_DB_PORT=6543
+NEW_DB_PORT=6543
+
+OLD_DB_PASS=secret_password_here
+NEW_DB_PASS=secret_new_password_here
+
+#Script:
+# Default case for Linux sed, just use "-i"
+sedi=(-i)
+case "$(uname)" in
+  # For macOS, use two parameters
+  Darwin*) sedi=(-i "")
+esac
+
+pg_dump postgres://postgres:"$OLD_DB_PASS"@"$OLD_DB_URL":"$OLD_DB_PORT"/postgres \
+  --clean \
+  --if-exists \
+  --quote-all-identifiers \
+  --exclude-table-data 'storage.objects' \
+  --exclude-schema 'extensions|graphql|graphql_public|net|pgbouncer|pgsodium|pgsodium_masks|realtime|supabase_functions|storage|pg_*|information_schema' \
+  --schema '*' > dump.sql 
+
+sed "${sedi[@]}" -e 's/^DROP SCHEMA IF EXISTS "auth";$/-- DROP SCHEMA IF EXISTS "auth";/' dump.sql
+sed "${sedi[@]}" -e's/^DROP SCHEMA IF EXISTS "storage";$/-- DROP SCHEMA IF EXISTS "storage";/' dump.sql
+sed "${sedi[@]}" -e 's/^CREATE SCHEMA "auth";$/-- CREATE SCHEMA "auth";/' dump.sql
+sed "${sedi[@]}" -e 's/^CREATE SCHEMA "storage";$/-- CREATE SCHEMA "storage";/' dump.sql
+sed "${sedi[@]}" -e 's/^ALTER DEFAULT PRIVILEGES FOR ROLE "supabase_admin"/-- ALTER DEFAULT PRIVILEGES FOR ROLE "supabase_admin"/' dump.sql
+psql postgres://postgres:"$NEW_DB_PASS"@"$NEW_DB_URL":"$NEW_DB_PORT"/postgres --file dump.sql 
